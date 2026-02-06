eab0a3cda960ea270a77e08080724098f398d861
winniethong <winniethong06@gmail.com>
2026-02-05 17:38:16 -0800

    01 functions practivce

--- js/01-functions-practice.js
+++ js/01-functions-practice.js
@@ -1,7 +1,33 @@
 //STEP 1
+function halfNumber(num) {
+    const result = num/2;
+    console.log(`Half of ${num} is ${result}.`);
+    return result;
+}
 
 //STEP 2
+function squareNumber(num) {
+    const result = num*num;
+    console.log(`The result of squaring the number ${num} is ${result}.`);
+    return result;
+}
+  //STEP 3
+function percentOf(num1, num2) {
+    const result = (num1/num2)* 100;
+    console.log(`${num1} is ${result}% of ${num2}.`);
+    return result;
+}
+  //STEP 4
+function findModulus(num1, num2) {
+    const result = num2%num1;
+    console.log(`${result} is the modulus of ${num1} and ${num2}.`);
+    return result;
+}
+  
+//making the calls
+  halfNumber(5);
+  squareNumber(3);
+  percentOf(2, 4);
+  findModulus(4, 10);
 
-//STEP 3
 
-//STEP 4
